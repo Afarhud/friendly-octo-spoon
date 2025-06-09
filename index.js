6d623b516953bee40e1db0a2bb734eb1858332a3
sefidprog <sefid.dp@gmail.com>
2025-06-09 03:05:35 -0800

    change code

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-module.exports = async (req, res) => {
-    // اگر درخواست GET باشد
-    if (req.method === 'GET') {
-        return res.json({
-            message: "این یک پاسخ تستی از Appwrite است!",
-            receivedData: req.query
-        });
-    }
+module.exports = async ({ req, res }) => {
+    try {
+        // برای درخواست‌های GET
+        if (req.method === 'GET') {
+            return res.send({
+                message: "این یک پاسخ تستی از Appwrite است!",
+                receivedData: req.query || {}
+            }, 200);
+        }
 
-    // اگر درخواست POST باشد
-    if (req.method === 'POST') {
-        return res.json({
-            message: "داده‌های POST دریافت شد!",
-            receivedData: req.payload
-        });
-    }
+        // برای درخواست‌های POST
+        if (req.method === 'POST') {
+            return res.send({
+                message: "داده‌های POST دریافت شد!",
+                receivedData: req.body || {}
+            }, 200);
+        }
 
-    // برای سایر انواع درخواست
-    return res.json({
-        message: "سلام از تابع Appwrite!",
-        method: req.method,
-        query: req.query,
-        payload: req.payload
-    });
+        // برای سایر متدها
+        return res.send({
+            message: "سلام از تابع Appwrite!",
+            method: req.method,
+            query: req.query || {},
+            body: req.body || {}
+        }, 200);
+
+    } catch (error) {
+        console.error('Error:', error);
+        return res.send({
+            error: "خطای داخلی سرور",
+            details: error.message
+        }, 500);
+    }
 };
